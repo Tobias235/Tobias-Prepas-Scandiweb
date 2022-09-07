b3c459a46df2b2ae742bbdec45c0c9a81a81289f
Tobias235 <tobias.prepas@gmail.com>
2022-09-07 16:56:31 +0800

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import { Component } from "react";
+import NavigationBar from "./components/navigation/NavigationBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <NavigationBar />
+      </div>
+    );
+  }
 }
 
 export default App;
