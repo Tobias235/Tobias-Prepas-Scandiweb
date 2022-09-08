fc24aa47ec8686a4306144dea988b3738b2322cd
Tobias235 <tobias.prepas@gmail.com>
2022-09-08 18:08:50 +0800

    Added category container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import styles from "./App.module.scss";
 import { Component } from "react";
 import NavigationBar from "./components/navigation/NavigationBar";
+import CategoryContainer from "./container/CategoryContainer/CategoryContainer";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
         <NavigationBar />
+        <CategoryContainer />
       </div>
     );
   }
