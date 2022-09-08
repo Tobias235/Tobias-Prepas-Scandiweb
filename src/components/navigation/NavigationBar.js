d6523c928ffc0500ac382d4f568108741c920c42
Tobias235 <tobias.prepas@gmail.com>
2022-09-08 18:11:37 +0800

    Update NavigationBar.js

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -2,7 +2,6 @@ import { Component } from "react";
 import NavigationLinks from "./NavigationLinks/NavigationLinks";
 import NavigationLogo from "./NavigationLogo/NavigationLogo";
 import NavigationCurrency from "./NavigationCurrencyOption/NavigationCurrency";
-import NavigationCartIcon from "./NavigationCart/NavigationCartIcon";
 import styles from "./NavigationBar.module.scss";
 
 class NavigationBar extends Component {
@@ -12,7 +11,6 @@ class NavigationBar extends Component {
         <NavigationLinks />
         <NavigationLogo />
         <NavigationCurrency />
-        <NavigationCartIcon />
       </nav>
     );
   }
