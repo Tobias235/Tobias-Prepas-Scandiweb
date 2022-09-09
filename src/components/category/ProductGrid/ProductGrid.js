266989c5fbc7088e5a1e2da831e8809f7ac06c35
Tobias235 <tobias.prepas@gmail.com>
2022-09-09 17:37:36 +0800

    Update ProductGrid.js

--- src/components/category/ProductGrid/ProductGrid.js
+++ src/components/category/ProductGrid/ProductGrid.js
@@ -1,14 +1,12 @@
 import { Component } from "react";
-import ProductCard from "../Products/ProductCard";
+import ProductCard from "../ProductCard/ProductCard";
+
 import styles from "./ProductGrid.module.scss";
 
 class ProductGrid extends Component {
   render() {
     return (
       <div className={styles.productGrid}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
         <ProductCard />
       </div>
     );
