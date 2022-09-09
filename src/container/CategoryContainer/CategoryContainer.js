f2fbb7b077c2efdcba0f919e409bb30d8a63e174
Tobias235 <tobias.prepas@gmail.com>
2022-09-09 17:37:17 +0800

    Updated with state
    Takes category from state and renders as title

--- src/container/CategoryContainer/CategoryContainer.js
+++ src/container/CategoryContainer/CategoryContainer.js
@@ -1,16 +1,23 @@
 import { Component } from "react";
+import { connect } from "react-redux";
 import ProductGrid from "../../components/category/ProductGrid/ProductGrid";
 import styles from "./CategoryContainer.module.scss";
 
 class CategoryContainer extends Component {
   render() {
+    const { category } = this.props;
+
     return (
       <div className={styles.categoryContainer}>
-        <h1>Category Name</h1>
+        <h1>{category}</h1>
         <ProductGrid />
       </div>
     );
   }
 }
 
-export default CategoryContainer;
+const mapStateToProps = (state) => ({
+  category: state.category,
+});
+
+export default connect(mapStateToProps)(CategoryContainer);
