521ac47df88c237a6a0e45dbaedfee92c6d036c0
Tobias235 <tobias.prepas@gmail.com>
2022-09-09 17:36:08 +0800

    Created cart icon component

---
+++ src/components/category/ProductCartIcon/CartIcon.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import styles from "./CartIcon.module.scss";
+import cartButton from "../../../assets/images/productCartIcon.svg";
+
+class CartIcon extends Component {
+  render() {
+    return (
+      <div className={styles.cartIcon}>
+        <img
+          src={cartButton}
+          alt="Add to cart button"
+          className={`${styles.showCartButton} ${this.props.className}`}
+        />
+      </div>
+    );
+  }
+}
+
+export default CartIcon;
