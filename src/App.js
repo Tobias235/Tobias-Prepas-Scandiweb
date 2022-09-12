10c27592bca1b6a535c8798f11775c6d6dabd18b
Tobias235 <tobias.prepas@gmail.com>
2022-09-12 11:54:10 +0800

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import styles from "./App.module.scss";
 import { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import styles from "./App.module.scss";
 import NavigationBar from "./components/navigation/NavigationBar";
-import CategoryContainer from "./container/CategoryContainer/CategoryContainer";
+import Category from "./container/CategoryContainer/Category";
+import Product from "./container/ProductContainer/Product";
+import Cart from "./container/CartContainer/Cart";
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <NavigationBar />
-        <CategoryContainer />
-      </div>
+      <>
+        <Router>
+          <NavigationBar />
+          <Route exact path="/category/:name">
+            <Category />
+          </Route>
+          <Route exact path="product/:productId">
+            <Product />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+        </Router>
+      </>
     );
   }
 }
