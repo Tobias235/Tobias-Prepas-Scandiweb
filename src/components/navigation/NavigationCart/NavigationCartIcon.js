52e6a6b616b5b19c083f573fe3e80b23997b3805
Tobias235 <tobias.prepas@gmail.com>
2022-09-12 19:35:05 +0800

    Added route to cart

--- src/components/navigation/NavigationCart/NavigationCartIcon.js
+++ src/components/navigation/NavigationCart/NavigationCartIcon.js
@@ -1,13 +1,19 @@
 import { Component } from "react";
+import { Link } from "react-router-dom";
 import styles from "./NavigationCartIcon.module.scss";
 import cartIcon from "../../../assets/images/cart.svg";
 
 class NavigationCartIcon extends Component {
   render() {
     return (
-      <div className={styles.navigationCartIcon}>
+      <Link
+        className={styles.navigationCartIcon}
+        to={{
+          pathname: `/cart`,
+        }}
+      >
         <img src={cartIcon} alt="Cart Icon" />
-      </div>
+      </Link>
     );
   }
 }
