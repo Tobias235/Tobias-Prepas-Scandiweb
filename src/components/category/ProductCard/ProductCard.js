3d0894c8f60e92de85b595b4ba9bda631f674891
Tobias235 <tobias.prepas@gmail.com>
2022-09-12 11:55:50 +0800

    Added filter and routes
    Filter for click on specific category and route to product page

--- src/components/category/ProductCard/ProductCard.js
+++ src/components/category/ProductCard/ProductCard.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { Query } from "@apollo/client/react/components";
 import { gql } from "@apollo/client";
+import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.scss";
 import CartIcon from "../ProductCartIcon/CartIcon";
 import OutOfStock from "../OutOfStock/OutOfStock";
@@ -35,21 +36,29 @@ const GET_PRODUCTS = gql`
     }
   }
 `;
-
 class ProductCard extends Component {
   render() {
-    const { currency } = this.props;
+    const { currency, category } = this.props;
     return (
       <Query query={GET_PRODUCTS}>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading…</p>;
           if (error) return <p>Error :(</p>;
-          return data.category.products.map((product) => (
-            <div
+          const products =
+            category === "all"
+              ? data.category.products
+              : Array.from(data.category.products).filter(
+                  (cat) => cat.category === category
+                );
+          return products.map((product) => (
+            <Link
               className={`${styles.productCard} ${
                 product.inStock ? null : styles.noStock
               }`}
               key={product.id}
+              to={{
+                pathname: `/product/${product.id}`,
+              }}
             >
               <img
                 src={product.gallery[0]}
@@ -66,7 +75,7 @@ class ProductCard extends Component {
                     ` ${cur.currency.symbol}${cur.amount}`
                 )}
               </span>
-            </div>
+            </Link>
           ));
         }}
       </Query>
@@ -76,6 +85,7 @@ class ProductCard extends Component {
 
 const mapStateToProps = (state) => ({
   currency: state.currency,
+  category: state.category,
 });
 
 export default connect(mapStateToProps)(ProductCard);
