5653296701243d94ff34e04d702418554535c988
Tobias235 <tobias.prepas@gmail.com>
2022-09-12 13:14:06 +0800

    Fixed redirect on pageload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import styles from "./App.module.scss";
 import NavigationBar from "./components/navigation/NavigationBar";
 import Category from "./container/CategoryContainer/Category";
 import Product from "./container/ProductContainer/Product";
 import Cart from "./container/CartContainer/Cart";
 
+import { connect } from "react-redux";
 class App extends Component {
   render() {
     return (
       <>
         <Router>
           <NavigationBar />
-          <Route exact path="/category/:name">
+          <Route exact path="/:name">
             <Category />
           </Route>
           <Route exact path="product/:productId">
@@ -21,10 +22,14 @@ class App extends Component {
           <Route exact path="/cart">
             <Cart />
           </Route>
+          <Redirect from="*" to="/all" />
         </Router>
       </>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  category: state.category,
+});
+export default connect(mapStateToProps)(App);
