06b080479237343099be2e0da98a293d2b0ee14d
Tobias235 <tobias.prepas@gmail.com>
2022-09-15 12:41:35 +0800

    Update NavigationCartIcon.js

--- src/components/navigation/NavigationCart/NavigationCartIcon.js
+++ src/components/navigation/NavigationCart/NavigationCartIcon.js
@@ -1,19 +1,19 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./NavigationCartIcon.module.scss";
 import cartIcon from "../../../assets/images/cart.svg";
 
 class NavigationCartIcon extends Component {
   render() {
     return (
-      <Link
+      <NavLink
         className={styles.navigationCartIcon}
         to={{
-          pathname: `/cart`,
+          pathname: `/cart/${Math.floor(Math.random() * 1000000)}`,
         }}
       >
         <img src={cartIcon} alt="Cart Icon" />
-      </Link>
+      </NavLink>
     );
   }
 }
