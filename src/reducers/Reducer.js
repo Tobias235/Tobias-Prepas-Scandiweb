595314eaecd58e1dcaf403466bb28cb457b147ae
Tobias235 <tobias.prepas@gmail.com>
2022-09-22 17:09:59 +0800

    Update Reducer.js
    Added reducer for quantity, delete product and active attributes

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -3,6 +3,7 @@ const initialState = {
   category: "all",
   productId: null,
   cart: [],
+  activeAttributes: [],
 };
 
 const Reducer = (state = initialState, action) => {
@@ -15,6 +16,38 @@ const Reducer = (state = initialState, action) => {
       return { ...state, productId: action.payload };
     case "SET_ADD_CART":
       return { ...state, cart: action.payload };
+    case "SET_ACTIVE_ATTRIBUTES":
+      return { ...state, activeAttributes: action.payload };
+    case "SET_INCREMENT_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((cart) => {
+          if (cart.uniqueId === action.payload) {
+            let quantity = cart.quantity + 1;
+            return { ...cart, quantity };
+          }
+          return cart;
+        }),
+      };
+
+    case "SET_DECREMENT_QUANTITY":
+      return {
+        ...state,
+        cart: state.cart.map((cart) => {
+          if (cart.uniqueId === action.payload && cart.quantity > 1) {
+            let quantity = cart.quantity - 1;
+            return { ...cart, quantity };
+          }
+          return cart;
+        }),
+      };
+
+    case "SET_DELETE_PRODUCT":
+      return {
+        ...state,
+        cart: state.cart.filter((cart) => cart.uniqueId !== action.payload),
+      };
+
     default:
       return state;
   }
