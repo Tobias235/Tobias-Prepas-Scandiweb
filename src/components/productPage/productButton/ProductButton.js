831178dbe8ea4c4a7359081318c36efba587c3c7
Tobias235 <tobias.prepas@gmail.com>
2022-09-22 17:14:54 +0800

    Add to cart functionality

--- src/components/productPage/productButton/ProductButton.js
+++ src/components/productPage/productButton/ProductButton.js
@@ -1,20 +1,30 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import { setAddCart } from "../../../actions/actions";
 import styles from "./ProductButton.module.scss";
 
 class ProductButton extends Component {
   state = {
+    cartItem: [],
     cartArray: [],
+    countArray: [],
   };
+
   render() {
-    const { product, cart } = this.props;
+    const { product, activeAttributes, cart } = this.props;
+    const { cartItem, cartArray } = this.state;
 
     const handleAddToCart = () => {
-      const { cartArray } = this.state;
-
-      cartArray.push(...cart, product);
+      cartItem.push({
+        ...product,
+        activeAttributes: activeAttributes,
+        uniqueId: uuidv4(),
+        quantity: 1,
+      });
+      cartArray.push(...cart, ...cartItem);
       this.props.onAddToCart(cartArray);
+
       alert("Product added to cart");
     };
     return (
@@ -31,6 +41,7 @@ class ProductButton extends Component {
 
 const mapStateToProps = (state) => ({
   cart: state.cart,
+  activeAttributes: state.activeAttributes,
 });
 
 const mapDispatchToProps = (dispatch) => ({
