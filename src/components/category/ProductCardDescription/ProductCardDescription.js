6004500d3ea585378c0ce00b8beeac26f64f116a
Tobias235 <tobias.prepas@gmail.com>
2022-09-22 17:17:17 +0800

    Clean up
    Seperated from product card component

---
+++ src/components/category/ProductCardDescription/ProductCardDescription.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import styles from "./ProductCardDescription.module.scss";
+
+class ProductCardDescription extends Component {
+  render() {
+    const { product, currency } = this.props;
+
+    return (
+      <div className={styles.descriptionContainer}>
+        <span id={product.id} className={styles.productName}>
+          {product.name}
+        </span>
+        <span id={product.id} className={styles.productPrice}>
+          {product.prices.map(
+            (cur) =>
+              cur.currency.symbol === currency &&
+              ` ${cur.currency.symbol}${cur.amount}`
+          )}
+        </span>
+      </div>
+    );
+  }
+}
+export default ProductCardDescription;
