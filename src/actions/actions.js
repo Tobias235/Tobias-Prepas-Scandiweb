7389da330255b225df2385d6e7314977e99866d4
Tobias235 <tobias.prepas@gmail.com>
2022-09-22 17:09:31 +0800

    Added actions
    Actions for quantity, delete product and choosen active attributes

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -25,3 +25,31 @@ export const setAddCart = (cart) => {
     payload: cart,
   };
 };
+
+export const setActiveAttributes = (attribute) => {
+  return {
+    type: "SET_ACTIVE_ATTRIBUTES",
+    payload: attribute,
+  };
+};
+
+export const setIncementQuantity = (increment) => {
+  return {
+    type: "SET_INCREMENT_QUANTITY",
+    payload: increment,
+  };
+};
+
+export const setDecrementQuantity = (decrement) => {
+  return {
+    type: "SET_DECREMENT_QUANTITY",
+    payload: decrement,
+  };
+};
+
+export const setDeleteProduct = (product) => {
+  return {
+    type: "SET_DELETE_PRODUCT",
+    payload: product,
+  };
+};
