c7b5b7427e3c4eeb45e5783ba71b767c583cf3ae
Tobias235 <tobias.prepas@gmail.com>
2022-09-22 17:13:19 +0800

    Added functionality to gallery

--- src/components/cart/CartImage/CartImage.js
+++ src/components/cart/CartImage/CartImage.js
@@ -1,17 +1,63 @@
 import { Component } from "react";
+import { ReactComponent as ArrowLeft } from "../../../assets/images/arrowLeft.svg";
+import { ReactComponent as ArrowRight } from "../../../assets/images/arrowRight.svg";
 import styles from "./CartImage.module.scss";
 
 class CartImage extends Component {
+  state = {
+    currentImage: 0,
+  };
   render() {
-    const { gallery } = this.props;
+    const { product } = this.props;
+    const { currentImage } = this.state;
+
+    const showArrow = product.gallery.length - 1;
+
+    const handleNextImage = (e) => {
+      if (e.target.id === product.uniqueId && currentImage < showArrow) {
+        this.setState({ currentImage: currentImage + 1 });
+      } else if (
+        e.target.id === product.uniqueId &&
+        currentImage === showArrow
+      ) {
+        this.setState({ currentImage: 0 });
+      }
+    };
+
+    const handlePrevImage = (e) => {
+      if (e.target.id === product.uniqueId && currentImage === 0) {
+        this.setState({ currentImage: showArrow });
+      } else if (
+        e.target.id === product.uniqueId &&
+        currentImage <= showArrow
+      ) {
+        this.setState({ currentImage: currentImage - 1 });
+      }
+    };
+
     return (
-      <div className={styles.imageSlider} onClick={this.handleChangePicture}>
+      <div className={styles.imageSlider}>
         <img
-          src={gallery[0]}
+          src={product.gallery[currentImage]}
           alt="placeholder"
           className={`${styles.mainImage}`}
-          onClick={this.handleImageClick}
         />
+        {showArrow > 1 && (
+          <div className={styles.arrowContainer}>
+            <div>
+              <ArrowLeft
+                id={product.uniqueId}
+                className={styles.arrowButton}
+                onClick={handlePrevImage}
+              />
+              <ArrowRight
+                id={product.uniqueId}
+                className={styles.arrowButton}
+                onClick={handleNextImage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
