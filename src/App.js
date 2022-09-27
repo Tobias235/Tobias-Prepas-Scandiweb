6508bbad1ddcdad7a180d379415b84794c727502
Tobias235 <tobias.prepas@gmail.com>
2022-09-27 18:22:14 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ import NavigationBar from "./components/navigation/NavigationBar";
 import Category from "./container/CategoryContainer/Category";
 import Product from "./container/ProductContainer/Product";
 import Cart from "./container/CartContainer/Cart";
-
-import { connect } from "react-redux";
+import MiniCartContainer from "./container/MiniCartContainer/MiniCartContainer";
 class App extends Component {
   render() {
     return (
       <>
-        <Router>
+        <Router className={styles.app}>
           <NavigationBar />
+          <MiniCartContainer />
           <Route exact path="/:name">
             <Category />
           </Route>
           <Route exact path="/product/:productId">
             <Product />
           </Route>
-          <Route exact path="/cart">
+          <Route exact path="/cart/:number">
             <Cart />
           </Route>
           <Redirect from="*" to="/all" />
@@ -29,7 +29,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  category: state.category,
-});
-export default connect(mapStateToProps)(App);
+export default App;
