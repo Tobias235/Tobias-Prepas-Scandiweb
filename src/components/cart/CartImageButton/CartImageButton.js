f3aec4ab82c49f0440bac73bf0cf73fc32cbd6b3
Tobias235 <tobias.prepas@gmail.com>
2022-09-27 18:22:01 +0800

    Image slider buttons

---
+++ src/components/cart/CartImageButton/CartImageButton.js
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import arrowLeft from "../../../assets/images/arrowLeft.svg";
+import arrowRight from "../../../assets/images/arrowRight.svg";
+import styles from "./CartImageButton.module.scss";
+
+class CartImageButton extends Component {
+  render() {
+    const { product, handlenextimage } = this.props;
+
+    const showArrow = product.gallery.length;
+
+    return (
+      <div className={styles.arrowContainer}>
+        <div>
+          {showArrow > 1 && (
+            <img
+              src={arrowLeft}
+              alt="Arrow icon to render next one"
+              className={styles.arrowButton}
+              id={product.uniqueId}
+              handlenextimage={handlenextimage}
+            />
+          )}
+          {showArrow > 1 && (
+            <img
+              src={arrowRight}
+              alt="Arrow icon to render next one"
+              className={styles.arrowButton}
+              id={product.uniqueId}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CartImageButton;
