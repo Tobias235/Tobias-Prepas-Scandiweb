1226071adcf796c0974de213dfc6b5bbdd6b9a02
Tobias235 <tobias.prepas@gmail.com>
2022-09-27 18:20:49 +0800

    added styling props

--- src/components/productPage/productPrice/ProductPrice.js
+++ src/components/productPage/productPrice/ProductPrice.js
@@ -5,12 +5,13 @@ class ProductPrice extends Component {
   render() {
     const { product, currency } = this.props;
     return (
-      <span className={styles.price}>
-        Price:
+      <span className={`${styles.price} ${this.props.className}`}>
+        {!this.props.miniCart && "Price:"}
         {product.prices.map((cur) => {
           return (
             cur.currency.symbol === currency && (
-              <span className={styles.amount} key={cur.currency.symbol}>
+              <span className={`${styles.amount}`} key={cur.currency.symbol}>
+                {/* ${this.props.className} */}
                 {cur.currency.symbol}
                 {cur.amount}
               </span>
--- src/components/productPage/productTitles/ProductTitles.js
+++ src/components/productPage/productTitles/ProductTitles.js
@@ -4,7 +4,7 @@ class ProductTitles extends Component {
   render() {
     const { product } = this.props;
     return (
-      <div className={styles.productTitles}>
+      <div className={`${styles.productTitles} ${this.props.className}`}>
         <h1>{product.brand}</h1>
         <h3>{product.name}</h3>
       </div>
