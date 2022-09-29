e26ba69d6f7cce9b14ef0f9219a89b240e6451da
Tobias235 <tobias.prepas@gmail.com>
2022-09-29 21:12:13 +0800

    Update MiniCartTotal.js
    Fetching the total value from function located in utils folder

--- src/components/miniCart/MiniCartTotal/MiniCartTotal.js
+++ src/components/miniCart/MiniCartTotal/MiniCartTotal.js
@@ -1,37 +1,16 @@
 import { Component } from "react";
+import { HandleProductTotalValues } from "../../../utils/HandleProductTotalValues";
 import styles from "./MiniCartTotal.module.scss";
 
 class MiniCartTotal extends Component {
   state = {
     total: 0,
-    totalArray: [],
   };
 
   handleTotalValue = () => {
-    const { totalArray } = this.state;
     const { cart, currency } = this.props;
-    let totalValue;
-    let initial;
-
-    this.setState({
-      total: 0,
-      totalArray: [],
-    });
-
-    cart.map((cart) => {
-      cart.prices.map((price) => {
-        if (price.currency.symbol === currency) {
-          totalArray.push(price.amount * cart.quantity);
-          initial = 0;
-          totalValue = totalArray.reduce(
-            (previousValue, currentValue) => previousValue + currentValue,
-            initial
-          );
-        }
-        return this.setState({ total: totalValue });
-      });
-      return cart;
-    });
+    let result = HandleProductTotalValues(cart, currency);
+    this.setState({ total: result[0].total });
   };
 
   componentDidMount() {
@@ -55,7 +34,7 @@ class MiniCartTotal extends Component {
       <div className={styles.miniCartTotal}>
         <span className={styles.total}>Total:</span>
         <span className={styles.amount}>
-          {currency} {total}
+          {currency} {total.toFixed(2)}
         </span>
       </div>
     );
