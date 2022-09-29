cc2a7d656ab158aeaacfde2d959fdc356a1d6a22
Tobias235 <tobias.prepas@gmail.com>
2022-09-29 18:48:19 +0800

    Update MiniCartButton.js
    Added alert to checkout button and close modal when clicked

--- src/components/miniCart/MiniCartButton/MiniCartButton.js
+++ src/components/miniCart/MiniCartButton/MiniCartButton.js
@@ -1,21 +1,37 @@
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 import styles from "./MiniCartButton.module.scss";
+import { setCheckOut, setShowMiniCart } from "../../../actions/actions";
 
 class MiniCartButton extends Component {
+  handleCheckOut = () => {
+    this.props.onSetCheckOut();
+    alert("Your order has been placed!");
+  };
+
+  handleCloseModal = () => {
+    this.props.onSetShowCart(false);
+  };
+
   render() {
     return (
       <div className={styles.miniCartButton}>
         <NavLink
           type="button"
           className={styles.viewBag}
+          onClick={this.handleCloseModal}
           to={{
             pathname: `/cart/${Math.floor(Math.random() * 1000000)}`,
           }}
         >
           VIEW BAG
         </NavLink>
-        <button type="button" className={styles.checkout}>
+        <button
+          type="button"
+          className={styles.checkout}
+          onClick={this.handleCheckOut}
+        >
           CHECK OUT
         </button>
       </div>
@@ -23,4 +39,9 @@ class MiniCartButton extends Component {
   }
 }
 
-export default MiniCartButton;
+const mapDispatchToProps = (dispatch) => ({
+  onSetCheckOut: () => dispatch(setCheckOut()),
+  onSetShowCart: (showCart) => dispatch(setShowMiniCart(showCart)),
+});
+
+export default connect(null, mapDispatchToProps)(MiniCartButton);
