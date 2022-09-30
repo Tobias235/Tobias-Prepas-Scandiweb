43eeea02a6082cf7cac08b8fff42f5737ee98dbb
Tobias235 <tobias.prepas@gmail.com>
2022-09-30 18:23:52 +0800

    Update CartOrderButton.js

--- src/components/cart/CartOrderButton/CartOrderButton.js
+++ src/components/cart/CartOrderButton/CartOrderButton.js
@@ -1,10 +1,24 @@
 import { Component } from "react";
+import { connect } from "react-redux";
+import { setCheckOut } from "../../../actions/actions";
 import styles from "./CartOrderButton.module.scss";
 
 class CartOrderButton extends Component {
   render() {
-    return <button className={styles.cartOrderButton}>Order</button>;
+    const handleCheckOut = () => {
+      this.props.onSetCheckOut();
+      alert("Your order has been placed!");
+    };
+    return (
+      <button className={styles.cartOrderButton} onClick={handleCheckOut}>
+        Order
+      </button>
+    );
   }
 }
 
-export default CartOrderButton;
+const mapDispatchToProps = (dispatch) => ({
+  onSetCheckOut: () => dispatch(setCheckOut()),
+});
+
+export default connect(null, mapDispatchToProps)(CartOrderButton);
