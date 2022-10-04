987c91edf0ac3f33b93a69fbe5b5d7760f3cbebd
Tobias235 <tobias.prepas@gmail.com>
2022-10-04 21:58:09 +0800

    added props for disabled

--- src/components/utils/Button/Button.js
+++ src/components/utils/Button/Button.js
@@ -8,6 +8,7 @@ class Button extends Component {
         type="button"
         className={`${styles.button} ${this.props.className}`}
         onClick={this.props.onClick}
+        disabled={this.props.disabled}
       >
         {this.props.text}
       </button>
