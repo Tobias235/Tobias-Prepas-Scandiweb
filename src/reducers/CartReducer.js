2472087ea68b2faf182f8efb7bd2ae7ccf163add
Tobias235 <tobias.prepas@gmail.com>
2023-03-16 22:39:55 +0100

    moved logic to redux reducer

--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -1,53 +1,111 @@
+import { v4 as uuidv4 } from "uuid";
+
 const initialState = {
-  cart: [],
+  cartItems: [],
 };
 
-const CartReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "SET_ADD_CART":
-      return { ...state, cart: action.payload };
-    case "SET_INCREMENT_QUANTITY":
+const CartReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "ADD_PRODUCT_TO_CART": {
+      const { product, selectedAttributes } = payload;
+      const { cartItems } = state;
+      const { id, attributes } = product;
+
+      const defaultActiveAttributes = attributes.reduce((acc, attribute) => {
+        const value = attribute?.items?.find((item) => !!item.value)?.value;
+        return value ? { ...acc, [attribute.name]: value } : acc;
+      }, {});
+
+      console.log(defaultActiveAttributes);
+
+      const itemExists = cartItems?.some(
+        (item) =>
+          item?.id === id &&
+          attributesMatch(
+            item?.activeAttributes,
+            selectedAttributes || defaultActiveAttributes
+          )
+      );
+
+      const updatedCartItems = cartItems?.map((item) => {
+        if (
+          item?.id === id &&
+          attributesMatch(
+            item?.activeAttributes,
+            selectedAttributes || defaultActiveAttributes
+          )
+        ) {
+          return { ...item, quantity: item.quantity + 1 };
+        } else {
+          return item;
+        }
+      });
+
+      if (itemExists) {
+        return { ...state, cartItems: updatedCartItems };
+      }
+
+      const newCartItem = {
+        ...product,
+        activeAttributes: {
+          ...defaultActiveAttributes,
+          ...(selectedAttributes || {}),
+        },
+        quantity: 1,
+        uniqueId: uuidv4(),
+      };
+
+      console.log(newCartItem);
+      return { ...state, cartItems: [...cartItems, newCartItem] };
+    }
+    case "INCREMENT_QUANTITY":
       return {
         ...state,
-        cart: state.cart.map((cart) => {
-          if (cart.uniqueId === action.payload) {
-            let quantity = cart.quantity + 1;
-            return { ...cart, quantity };
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.uniqueId === payload) {
+            let quantity = cartItem.quantity + 1;
+            return { ...cartItem, quantity };
           }
-          return cart;
+          return cartItem;
         }),
       };
-    case "SET_DECREMENT_QUANTITY":
+    case "DECREMENT_QUANTITY":
       return {
         ...state,
-        cart: state.cart.map((cart) => {
-          if (cart.uniqueId === action.payload && cart.quantity > 1) {
-            let quantity = cart.quantity - 1;
-            return { ...cart, quantity };
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.uniqueId === payload && cartItem.quantity > 1) {
+            let quantity = cartItem.quantity - 1;
+            return { ...cartItem, quantity };
           }
-          return cart;
+          return cartItem;
         }),
       };
-    case "SET_CHANGE_QUANTITY":
+    case "CHANGE_QUANTITY":
       return {
         ...state,
-        cart: state.cart.map((cart) => {
-          if (cart.uniqueId === action.payload) {
-            let quantity = cart.quantity + 1;
-            return { ...cart, quantity };
+        cartItems: state.cartItems.map((cartItem) => {
+          if (cartItem.uniqueId === payload) {
+            let quantity = cartItem.quantity + 1;
+            return { ...cartItem, quantity };
           }
-          return cart;
+          return cartItem;
         }),
       };
-    case "SET_DELETE_PRODUCT":
+    case "DELETE_PRODUCT":
       return {
         ...state,
-        cart: state.cart.filter((cart) => cart.uniqueId !== action.payload),
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.uniqueId !== payload
+        ),
       };
-    case "SET_CHECK_OUT":
+    case "RESET_CART":
       return { ...initialState };
     default:
       return state;
   }
 };
 export default CartReducer;
+
+const attributesMatch = (itemAttributes, targetAttributes) => {
+  return JSON.stringify(itemAttributes) === JSON.stringify(targetAttributes);
+};
