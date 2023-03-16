a20db682ff8c403c8a7d81ba060fdf82dcb4b4f2
Tobias235 <tobias.prepas@gmail.com>
2023-03-16 22:40:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { Provider } from "react-redux";
-import store from "./store";
+import store from "./Store";
 import "./index.module.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { PersistGate } from "redux-persist/integration/react";
 
 const client = new ApolloClient({
-  uri: "https://zesty-sunshine-366520.netlify.app/",
+  uri: process.env.REACT_APP_API_URL,
   cache: new InMemoryCache(),
   connectToDevTools: true,
 });
@@ -24,8 +23,3 @@ root.render(
     </Provider>
   </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
